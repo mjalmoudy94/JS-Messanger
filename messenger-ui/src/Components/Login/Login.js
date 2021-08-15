a2ad38f2f624fa3906163725a3de0ef307d46267
mohammad <m.jalmoudy94@gmail.com>
2021-08-15 07:40:34 +0430

    Adding Materialize

--- messenger-ui/src/Components/Login/Login.js
+++ messenger-ui/src/Components/Login/Login.js
@@ -1,4 +1,4 @@
-import './Login.css'
+// import './Login.css'
 import UiCore from "../../core/UiCore";
 
 const Login = (Props) => {
@@ -20,22 +20,47 @@ const Login = (Props) => {
     }
 
     return (
-        <div className='LoginComponent'>
-            <div className="main">
-                <p className="sign">Sign in</p>
-                <form className="form1">
-                    <input className="un " type="text" placeholder="Username" onChange={UpdateUserName}/>
-                    <input className="pass" type="password" placeholder="Password" onChange={UpdatePassword}/>
-                    <a className="submit" onClick={Connect}>Sign in</a>
-                    <p className="forgot">
-                        <a href="#">Forgot Password?</a>
-                    </p>
-                </form>
-            </div>
-            <p className={'MessengerStatus'}>
-                {Props.Status}
-            </p>
-        </div>
+        <>
+
+            <ul className="collapsible">
+                <li>
+                    <div className="collapsible-header">
+                        <i className="material-icons">filter_drama</i>
+                        First
+                        <span className="new badge">4</span></div>
+                    <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+                </li>
+                <li>
+                    <div className="collapsible-header">
+                        <i className="material-icons">place</i>
+                        Second
+                        <span className="badge">1</span></div>
+                    <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+                </li>
+            </ul>
+
+
+            <a className="btn-floating pulse"><i className="material-icons">menu</i></a>
+            <a className="btn-floating btn-large pulse"><i className="material-icons">cloud</i></a>
+            <a className="btn-floating btn-large cyan pulse"><i className="material-icons">edit</i></a>
+
+
+            {/*<div className="main">*/}
+            {/*    <p className="">Sign in</p>*/}
+            {/*    <form className="center-align">*/}
+            {/*        <input className="" type="text" placeholder="Username" onChange={UpdateUserName}/>*/}
+            {/*        <input className="" type="password" placeholder="Password" onChange={UpdatePassword}/>*/}
+
+            {/*        <a className="btn" onClick={Connect}><i className="material-icons right">cloud</i>button</a>*/}
+            {/*        <p className="forgot">*/}
+            {/*            <a href="#">Forgot Password?</a>*/}
+            {/*        </p>*/}
+            {/*    </form>*/}
+            {/*</div>*/}
+            {/*<p className={'MessengerStatus'}>*/}
+            {/*    {Props.Status}*/}
+            {/*</p>*/}
+        </>
     );
 
 };
